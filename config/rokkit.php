ce61829d32bf3f8690dbc870e61982e51d6217be
David Grzyb <grzybdavid@gmail.com>
2020-02-29 17:49:18 -0500

    fixed storage directory

--- config/rokkit.php
+++ config/rokkit.php
@@ -2,5 +2,5 @@
 
 return [
     'default_domain' => env('DEFAULT_DOMAIN'),
-    'advertisement_storage_directory' => 'advertisements',
+    'advertisement_storage_directory' => env('ADVERTISEMENT_STORAGE_DIRECTORY'),
 ];
