68d58613bf4c8814c96b2b48412fca90163c55cd
David Grzyb <grzybdavid@gmail.com>
2020-03-02 22:02:50 -0500

    added favicon

--- resources/views/layouts/minimal.blade.php
+++ resources/views/layouts/minimal.blade.php
@@ -14,19 +14,13 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Icons -->
-        <link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
-        <link rel="icon" sizes="192x192" type="image/png" href="{{ asset('media/favicons/favicon-192x192.png') }}">
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('media/favicons/apple-touch-icon-180x180.png') }}">
+        <link rel="shortcut icon" href="{{ asset('storage/favicon.png') }}">
 
         <!-- Fonts and Styles -->
         @yield('css_before')
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700">
         <link rel="stylesheet" id="css-main" href="{{ mix('/css/codebase.css') }}">
-        <!-- <link rel="stylesheet" id="css-theme" href="{{ mix('/css/themes/earth.css') }}"> -->
-        <!-- <link rel="stylesheet" id="css-theme" href="{{ mix('/css/themes/flat.css') }}"> -->
 
-        <!-- You can include a specific file from public/css/themes/ folder to alter the default color theme of the template. eg: -->
-        <!-- <link rel="stylesheet" id="css-theme" href="{{ mix('/css/themes/corporate.css') }}"> -->
         @yield('css_after')
 
         <!-- Scripts -->
