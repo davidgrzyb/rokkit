9a80ae30e125abb85a80ee960c5ce7e27efe5612
David Grzyb <grzybdavid@gmail.com>
2020-03-10 20:38:06 -0400

    fixed Cloudways Api Interface import bug

--- app/Http/Api/CloudwaysApiInterface.php
+++ app/Http/Api/CloudwaysApiInterface.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Api;
 
+use GuzzleHttp\{
+    RequestOptions,
+    ClientInterface,
+};
+
 interface CloudwaysApiInterface
 {
     public function __construct(ClientInterface $client, string $clientEmail, string $clientKey);
-    function getToken();
     public function addDomain();
     public function addCertificate();
 }
